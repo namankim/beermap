548c2985f047264a4f47cc1a9c4102ebf22958d6
namankim <namankim@guhubmail.com>
2026-05-05 21:57:49 +0900

    Render home page dynamically

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { BeerMapScreen } from "@/components/beer-map-screen";
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const spots = await prisma.beerSpot.findMany({
     orderBy: {
